a775cf16212925a2102a5248b541933b9933c2ed
HocITnha <nguyennguyenle0009@gmail.com>
2025-08-21 17:53:58 +0700

    Optimize rendering and map loading

--- src/game/util/UtilityTool.java
+++ src/game/util/UtilityTool.java
@@ -3,6 +3,7 @@ package game.util;
 import java.awt.Graphics2D;
 import java.awt.image.BufferedImage;
 
+import game.entity.Player;
 import game.main.GamePanel;
 
 public class UtilityTool {
@@ -16,10 +17,17 @@ public class UtilityTool {
     
     //Objet được vẽ cách ngoài biên màn hình 2 * gp.getTileSize()
     public static boolean isInsidePlayerView(int worldX, int worldY, GamePanel gp) {
-        return worldX + 4 * gp.getTileSize() > gp.getPlayer().getWorldX() - gp.getPlayer().getScreenX() && 
-	           worldX - 4 * gp.getTileSize() < gp.getPlayer().getWorldX() + gp.getPlayer().getScreenX() &&
-	           worldY + 4 * gp.getTileSize() > gp.getPlayer().getWorldY() - gp.getPlayer().getScreenY() && 
-	           worldY - 4 * gp.getTileSize() < gp.getPlayer().getWorldY() + gp.getPlayer().getScreenY();
+        Player player = gp.getPlayer();
+        int tileSize = gp.getTileSize();
+        int offset = tileSize * 4;
+        int playerWorldX = player.getWorldX();
+        int playerWorldY = player.getWorldY();
+        int playerScreenX = player.getScreenX();
+        int playerScreenY = player.getScreenY();
+        return worldX + offset > playerWorldX - playerScreenX &&
+                   worldX - offset < playerWorldX + playerScreenX &&
+                   worldY + offset > playerWorldY - playerScreenY &&
+                   worldY - offset < playerWorldY + playerScreenY;
     }
     
     public static int getXForCenterOfText(String text, GamePanel gamePanel, Graphics2D graphics2D) {
